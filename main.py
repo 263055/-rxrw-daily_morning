009d5404a6d273eb4553d196fa20d8e8728efcfd
263055 <106651745+263055@users.noreply.github.com>
2023-01-25 21:43:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,11 @@ today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
-
-birthday2 = os.environ['BIRTHDAY2']
+birthday2 = os.environ['MYBIRTHDAY']
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
-
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
-
 menses = os.environ['MENSES_DATE']
 
 def get_weather():
